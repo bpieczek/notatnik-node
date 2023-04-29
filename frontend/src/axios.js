bb3e03300029e5456040cdaec816fe857933a0f9
bpieczek <bartosz.pieczek@pisz.to>
2023-04-29 14:40:04 +0200

    base url change

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.API_URL || "http://localhost:3001/api"
+  baseURL: process.env.API_URL || "https://api-notes-5azq.onrender.com",
 });
 
-export default instance
+export default instance;
